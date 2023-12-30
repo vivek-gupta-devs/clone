7aca3f67f8a9e0baaa456f6ad725a292b599daa9
Vivek Gupta <vivek.gupta.devs@gmail.com>
2023-12-30 06:13:44 +0530

    database confi

---
+++ src/db/index.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = () => mongoose
+                    .connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+                    .then(() => console.log("Connection succcesfully establish with MongoDB"))
+                    .catch((err) => console.log("Connection [n] establish with MongoDB"));
+
+export default connectDB
