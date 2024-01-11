895b3ff76ef03da6d22d18558d7d592239cde77b
Vivek Gupta <vivek.gupta.devs@gmail.com>
2024-01-11 19:13:57 +0530

    Adding user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -142,11 +142,11 @@ const loginUser = asyncHandler( async(req,res) => {
 });
 
 const logoutUser = asyncHandler( async(req,res) => {
-    User.findByIdAndUpdate(
+    await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set : {
-                refreshToken: undefined
+            $unset : {
+                refreshToken: 1 // this removes the field from document
             }
         },{
             new : true
