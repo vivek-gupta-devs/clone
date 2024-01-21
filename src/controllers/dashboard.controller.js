6ec6ddfd5ec39a10e869b73922ec93fbedcb4f2c
Vivek Gupta <vivek.gupta.devs@gmail.com>
2024-01-21 11:25:16 +0530

    subscription method addition

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,17 +1,106 @@
 import mongoose from "mongoose"
 import {Video} from "../models/video.model.js"
+import {User} from "../models/user.model.js"
 import {Subscription} from "../models/subscription.model.js"
 import {Like} from "../models/like.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-const getChannelStats = asyncHandler(async (req, res) => {
+const getChannelStats = asyncHandler( async(req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    
+    const channelStats = await User.aggregate(
+      
+          [
+            {
+                $match:{
+                    _id: req.user._id,
+                }
+            },
+            
+            {
+                $lookup: {
+                  from: "tweets",
+                  localField: "_id",
+                  foreignField: "owner",
+                  as: "tweet"
+                }
+            },
+
+            {
+                $lookup: {
+                  from: "likes",
+                  localField: "_id",
+                  foreignField: "likedBy",
+                  as: "like"
+                }
+            },
+
+            {
+                $lookup: {
+                  from: "comments",
+                  localField: "_id",
+                  foreignField: "owner",
+                  as: "comment"
+                }
+            },
+
+            {
+                $project: {
+                    tweet: 1,
+                    comment: 1,
+                    like: 1
+                }
+            }
+
+
+
+        ]
+
+    )
+
+
+    return res.status(200).json(
+        new ApiResponse(200, channelStats, "Channel stats for user")
+    )
+
+   
+
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
     // TODO: Get all the videos uploaded by the channel
+
+    const videos = await User.aggregate(
+        [
+            {
+                $match: {
+                    _id: req.user?._id
+                }
+            },
+
+
+            {
+                $lookup:{
+                    from: "videos",
+                    localField: "_id",
+                    foreignField: "owner",
+                    as: "videoList"
+                  }
+            },
+
+            {
+                $project: {
+                    videoList: 1
+                }
+            }
+        ]
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200, videos, "Uploaded video by user")
+    )
 })
 
 export {
--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,16 +9,119 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+
+    const sub_channel = await Subscription.findOne(
+        {
+            channel: channelId,
+            subscriber: req.user._id
+        }
+    )
+
+
+    let status;
+
+    if(sub_channel){
+        status = await Subscription.deleteOne(sub_channel._id);    
+    }else{
+        status = await Subscription.create(
+            {
+                channel: channelId,
+                subscriber: req.user._id
+            }
+        )
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,status,"subscription status got updated.")
+    )
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    const { subscriberId } = req.params
+    
+    const subscriber = await Subscription.aggregate(
+        [
+            {
+              $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+              }
+            },
+          
+            {
+              $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channels",
+               
+              }
+            },
+          
+            {
+              $project: {
+                channels: 1,
+                _id: 0
+              }
+            }
+          
+            
+          ]
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200,subscriber,"All channels")
+    )
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params
+
+    const {channelId} = req.params
+
+    const subscribers = await Subscription.aggregate(
+        [
+            {
+              $match: {
+                channel: new mongoose.Types.ObjectId(channelId),
+              }
+            },
+          
+            {
+              $lookup: {
+                from: "users",
+                localField: "subscriber",
+                foreignField: "_id",
+                as: "subscriber",
+                pipeline:[
+                  {
+                          $project: {
+                              fullName: 1,
+                              username: 1,
+                              email: 1,
+                            _id: 0
+          
+                          }
+                      }
+                ]
+              }
+            },
+          
+            {
+              $project: {
+                    subscriber: 1,
+                    _id: 0
+                }
+            
+            }
+          ]
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200,subscribers,"Subscriber for channel")
+    )
+
+   
 })
 
 export {
